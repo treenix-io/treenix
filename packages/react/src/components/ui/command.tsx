5d2d3cb6f3b15a078ea6e374003b209a73ac5f55
feddot2517 <feddot2517@gmail.com>
2026-04-10 16:59:33 +0300

    fix: type picker default height

--- packages/react/src/components/ui/command.tsx
+++ packages/react/src/components/ui/command.tsx
@@ -60,13 +60,13 @@ function CommandInput({
   return (
     <div
       data-slot="command-input-wrapper"
-      className="flex h-9 items-center gap-2 border-b px-3"
+      className="flex h-14 items-center gap-2 border-b px-4 py-3"
     >
       <SearchIcon className="size-4 shrink-0 opacity-50" />
       <CommandPrimitive.Input
         data-slot="command-input"
         className={cn(
-          "flex h-10 w-full rounded-md bg-transparent py-3 text-sm outline-hidden placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50",
+          "flex h-8 w-full rounded-md bg-transparent text-sm outline-hidden placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         {...props}
