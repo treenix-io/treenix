502b2161a979c65bbaf0c1cfb99e4ed38482ee62
kriz <krizka@gmail.com>
2026-05-03 20:12:36 +0300

    fix dir view

--- packages/react/src/mods/editor-ui/client.ts
+++ packages/react/src/mods/editor-ui/client.ts
@@ -1,6 +1,7 @@
 import './default-view';
 import './default-edit';
 import './layout-view';
+import './dir-view';
 import './user-view';
 import './list-items';
 import { registerFormFields } from './form-fields';
