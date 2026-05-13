ee24b02ddb6a1021098567b1fcda11e6df7640e6
kriz <krizka@gmail.com>
2026-05-13 15:27:39 +0300

    feat(core): re-export comp symbols from @treenx/core root

--- core/src/index.ts
+++ core/src/index.ts
@@ -1,1 +1,15 @@
 export * from './core/index';
+export {
+  getCtx,
+  registerType,
+  registerActions,
+  setComponent,
+  newComponent,
+  getDefaults,
+  type ExecCtx,
+  type TypeClass,
+  type Raw,
+  type Actions,
+  type PortDecl,
+  type TypeProxy,
+} from './comp/index';
